9f04bebf7aabcd8357cb82f4ae32ef58a92a5baf
Kaveramaison <hello@kaveramaison.com>
2026-06-16 11:48:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
-from routes.meta_test import router as meta_test_router
 
 app = FastAPI(
     title="AI Marketing OS Backend",
     version="0.1.0"
 )
-
-app.include_router(meta_test_router)
 
 @app.get("/")
 def home():
